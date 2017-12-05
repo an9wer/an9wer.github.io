f81df67bacb43694b9e10e5f9e071c63e8aeca83
an9wer <an9wer@gmail.com>
2017-12-05 21:32:05 +0800

    Try scrolling to bottom and then loading more

--- demo.py
+++ demo.py
@@ -10,5 +10,16 @@ def index():
 def static(path):
     return static_file(path, root="./static/")
 
+@route("/api/load/")
+def load():
+    return {
+        "items": [
+            {"datetime": "2017-11-26", "mind": "I'll get it" },
+            {"datetime": "2017-11-30", "mind": "demo it."},
+            {"datetime": "2017-12-03", "mind": "dame cool."},
+            {"datetime": "2017-12-05", "mind": "scroll to bottom load more"},
+        ]
+    }
+
 if __name__ == "__main__":
     run(host="0.0.0.0", port="8080", debug=True)
