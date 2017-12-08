68286bc74afe911e86e90a5c13afedacc15838a0
an9wer <an9wer@gmail.com>
2017-12-08 19:55:57 +0800

    Add ease loading

--- demo.py
+++ demo.py
@@ -18,6 +18,8 @@ def load():
             {"datetime": "2017-11-30", "mind": "demo it."},
             {"datetime": "2017-12-03", "mind": "dame cool."},
             {"datetime": "2017-12-05", "mind": "scroll to bottom load more"},
+            {"datetime": "2017-12-07", "mind": "css transition"},
+            {"datetime": "2017-12-08", "mind": "ease loading"}
         ]
     }
 
